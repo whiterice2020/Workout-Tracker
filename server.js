2e46ab9fee621a60a624d49ce2ff0adace485ef6
Paul DelPadre <PDelPadre@rice.local>
2020-08-03 20:37:53 -0400

    updating mongo

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI, {
+// mongoose.connect("mongodb://localhost/workout", {
     useNewUrlParser: true,
     useFindAndModify: false
 });
